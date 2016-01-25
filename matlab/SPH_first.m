aafe1d0c6132b541eda8e914346a1fc94b66091f
ejuopperi <juopperiemil@gmail.com>
2016-01-25 11:35:02 +0100

    Add pressure and force fields to particle struct and rename location to position.

--- matlab/SPH_first.m
+++ matlab/SPH_first.m
@@ -14,9 +14,10 @@ s = struct(field1,value1)
 
 field1 = 'mass'; value1 = 1.0;
 field2 = 'density'; value2 = 1.2;
-field3 = 'viscosity'; value3 = 3.2;
-field4 = 'location'; value4 = [1.0 2.0];
-field5 = 'velocity'; value5 = [0.0 0.0];
+field3 = 'position'; value3 = [1.0 2.0];
+field4 = 'velocity'; value4 = [0.0 0.0];
+field5 = 'pressure'; value5 = 0.0;
+field6 = 'force'; value6 = [0.0 0.0];
 % Surface tension?
 
 particle = struct(...
@@ -34,7 +35,7 @@ for i = 1:100
    particles(i) = particle; 
    
    % Give each particle random velocity and position
-   particles(i).location = [rand*10 rand*10];
+   particles(i).position = [rand*10 rand*10];
    particles(i).velocity = [2*rand-1 2*rand-1];
 end
 
@@ -45,10 +46,11 @@ drawParticles(particles);
 %% Calculate Properties
 while waitforbuttonpress
   for k = 1:length(particles)
-    position = particles(k).location;
+    position = particles(k).position;
 
+    
     position = position + particles(k).velocity * dt;
-    particles(k).location = position;
+    particles(k).position = position;
   end
 
   clf;
--- matlab/drawParticles.m
+++ matlab/drawParticles.m
@@ -1,6 +1,6 @@
 function drawParticles(particles)
   for j = 1:length(particles)
-      pos = particles(j).location;
+      pos = particles(j).position;
       plot(pos(1), pos(2), 'ob');
       axis([0 10 0 10]);
       hold on
