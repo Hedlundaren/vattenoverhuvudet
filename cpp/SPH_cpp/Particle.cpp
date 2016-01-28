d9a685bb75948de9b3e0089c258790b14233ca08
Hedlundaren <hedlundaren93@hotmail.com>
2016-01-28 16:17:12 +0100

    Class particle init

---
+++ cpp/SPH_cpp/Particle.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+/**********************************
+* Class Particle               *
+***********************************/
+
+class Particle
+{
+public:
+
+    //Constructor
+    Particle();
+    ~Particle();
+
+    float position [3] = {0.0f, 0.0f, 0.0f};
+    float velocity [3] = {0.0f, 0.0f, 0.0f};
+    float force [3] = {0.0f, 0.0f, 0.0f};
+    float density = 0.0f;
+    float pressure = 0.0f;
+    float cs = 0.0f;
+
+protected:
+
+
+private:
+
+};
+
+Particle::Particle(){
+    position[0] = rand();
+    position[1] = rand();
+    position[2] = rand();
+}
+
+ostream& operator<<(ostream& out, const Particle& p)
+{
+    out << "x: " << p.position[0] << " y: " << p.position[1] << " z: " << p.position[2] << endl;
+
+    return out;
+}
+
+ int main(){
+
+    Particle* P = new Particle();
+
+    cout << *P;
+
+ return 0;
+ }
