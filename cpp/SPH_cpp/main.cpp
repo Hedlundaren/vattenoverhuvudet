4fceb30fca349028fb41ead7448ba3d4119dd775
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-01-28 21:28:25 +0100

    Fix OpenCL linking on Windows with Cuda SDK + gitignore build/

--- cpp/SPH_cpp/main.cpp
+++ cpp/SPH_cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -7,12 +8,109 @@
 #endif
 
 #include "Particle.h"
-#include "kernels.h"
 
 #include "glm/glm.hpp"
 #include "glm/ext.hpp"
 
-int main() {
+std::string GetPlatformName (cl_platform_id id)
+{
+    size_t size = 0;
+    clGetPlatformInfo (id, CL_PLATFORM_NAME, 0, nullptr, &size);
+
+    std::string result;
+    result.resize (size);
+    clGetPlatformInfo (id, CL_PLATFORM_NAME, size,
+                       const_cast<char*> (result.data ()), nullptr);
+
+    return result;
+}
+
+std::string GetDeviceName (cl_device_id id)
+{
+    size_t size = 0;
+    clGetDeviceInfo (id, CL_DEVICE_NAME, 0, nullptr, &size);
+
+    std::string result;
+    result.resize (size);
+    clGetDeviceInfo (id, CL_DEVICE_NAME, size,
+                     const_cast<char*> (result.data ()), nullptr);
+
+    return result;
+}
+
+void CheckError (cl_int error)
+{
+    if (error != CL_SUCCESS) {
+        std::cerr << "OpenCL call failed with error " << error << std::endl;
+        std::exit (1);
+    }
+}
+
+int main ()
+{
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetPlatformIDs.html
+    cl_uint platformIdCount = 0;
+    clGetPlatformIDs (0, nullptr, &platformIdCount);
+
+    if (platformIdCount == 0) {
+        std::cerr << "No OpenCL platform found" << std::endl;
+        return 1;
+    } else {
+        std::cout << "Found " << platformIdCount << " platform(s)" << std::endl;
+    }
+
+    std::vector<cl_platform_id> platformIds (platformIdCount);
+    clGetPlatformIDs (platformIdCount, platformIds.data (), nullptr);
+
+    for (cl_uint i = 0; i < platformIdCount; ++i) {
+        std::cout << "\t (" << (i+1) << ") : " << GetPlatformName (platformIds [i]) << std::endl;
+    }
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetDeviceIDs.html
+    cl_uint deviceIdCount = 0;
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, 0, nullptr,
+                    &deviceIdCount);
+
+    if (deviceIdCount == 0) {
+        std::cerr << "No OpenCL devices found" << std::endl;
+        return 1;
+    } else {
+        std::cout << "Found " << deviceIdCount << " device(s)" << std::endl;
+    }
+
+    std::vector<cl_device_id> deviceIds (deviceIdCount);
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_ALL, deviceIdCount,
+                    deviceIds.data (), nullptr);
+
+    for (cl_uint i = 0; i < deviceIdCount; ++i) {
+        std::cout << "\t (" << (i+1) << ") : " << GetDeviceName (deviceIds [i]) << std::endl;
+    }
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateContext.html
+    const cl_context_properties contextProperties [] =
+            {
+                    CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties> (platformIds [0]),
+                    0, 0
+            };
+
+    cl_int error = CL_SUCCESS;
+    cl_context context = clCreateContext (contextProperties, deviceIdCount,
+                                          deviceIds.data (), nullptr, nullptr, &error);
+    CheckError (error);
+
+    std::cout << "Context created" << std::endl;
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateCommandQueue.html
+    cl_command_queue queue = clCreateCommandQueue (context, deviceIds [0],
+                                                   0, &error);
+    CheckError (error);
+
+    // Here were ready to actually run the code
+
+    clReleaseCommandQueue (queue);
+
+    clReleaseContext (context);
+
     const glm::vec3 vec1(1.0f, 0.0f, 0.5f), vec2(0.0f, 1.0f, 0.5f);
     const glm::vec3 sum = vec1 + vec2;
 
@@ -23,5 +121,4 @@ int main() {
     Particle particles[n_Particles];
 
     std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
