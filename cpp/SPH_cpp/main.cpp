01d3beaf18da2ed664906b5a34ba381db0f15c0f
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-02-02 11:44:12 +0100

    Setup window creating structure in main

--- cpp/SPH_cpp/main.cpp
+++ cpp/SPH_cpp/main.cpp
@@ -14,18 +14,48 @@
 
 #include "opencl_context_info.hpp"
 
+#include "GLFW/glfw3.h"
+#include <unistd.h>
+
 int main() {
     PrintOpenClContextInfo();
 
-    const glm::vec3 vec1(1.0f, 0.0f, 0.5f), vec2(0.0f, 1.0f, 0.5f);
-    const glm::vec3 sum = vec1 + vec2;
+    const GLFWvidmode *vidmode;  // GLFW struct to hold information about the display
+    GLFWwindow *window;    // GLFW struct to hold information about the window
 
-    std::cout << glm::to_string(sum) << std::endl;
+    // Initialise GLFW
+    glfwInit();
 
-    const int n_Particles = 100;
+    // Determine the desktop size
+    vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-    Particle particles[n_Particles];
+    // Make sure we are getting a GL context of at least version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Exclude old legacy cruft from the context. We don't need it, and we don't want it.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    std::cout << "Hello, World!" << std::endl;
+    // Open a square window (aspect 1:1) to fill half the screen height
+    window = glfwCreateWindow(800, 600, "FluidSPH", NULL, NULL);
+    if (!window) {
+        glfwTerminate(); // No window was opened, so we can't continue in any useful way
+        return -1;
+    }
+
+    // Make the newly created window the "current context" for OpenGL
+    // (This step is strictly required, or things will simply not work)
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Exit if the ESC key is pressed (and also if the window is closed).
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(window, GL_TRUE);
+        }
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
