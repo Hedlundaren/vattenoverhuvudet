ff98a777541301635979fb59b9f86580fc22ee2d
adam <adam.alsegard@gmail.com>
2016-02-02 16:46:30 +0100

    Rendering static particles

--- cpp/SPH_cpp/main.cpp
+++ cpp/SPH_cpp/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <glm/glm.hpp>
 
 #include "GLFW/glfw3.h"
 
 #include "opencl_context_info.hpp"
 #include "rendering/ShaderProgram.hpp"
+#include "math/randomized.hpp"
 
 int main() {
     PrintOpenClContextInfo();
@@ -28,6 +30,36 @@ int main() {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    //Generate particles
+    const int n_particles = 5000;
+    std::vector <glm::vec3> positions = generate_uniform_vec3s(n_particles, -1, 1, -1, 1, -1, 1);
+    std::vector <glm::vec3> velocities = generate_uniform_vec3s(n_particles, -1, 1, -1, 1, -1, 1);
+
+    //Generate VBOs
+    GLuint pos_vbo = 0;
+    glGenBuffers (1, &pos_vbo);
+    glBindBuffer (GL_ARRAY_BUFFER, pos_vbo);
+    glBufferData (GL_ARRAY_BUFFER, n_particles * 3 * sizeof (float), positions.data(), GL_STATIC_DRAW);
+
+    GLuint vel_vbo = 0;
+    glGenBuffers (1, &vel_vbo);
+    glBindBuffer (GL_ARRAY_BUFFER, vel_vbo);
+    glBufferData (GL_ARRAY_BUFFER, n_particles * 3 * sizeof (float), velocities.data(), GL_STATIC_DRAW);
+
+    // Generate VAO with all VBOs
+    GLuint vao = 0;
+    glGenVertexArrays (1, &vao);
+    glBindVertexArray (vao);
+    glBindBuffer (GL_ARRAY_BUFFER, pos_vbo);
+    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL); //position
+    glBindBuffer (GL_ARRAY_BUFFER, vel_vbo);
+    glVertexAttribPointer (1, 3, GL_FLOAT, GL_FALSE, 0, NULL); //velocity
+
+    //How many attributes do we have? Enable them!
+    glEnableVertexAttribArray (0);
+    glEnableVertexAttribArray (1);
+
+
     ShaderProgram particlesShader("../shaders/particles.vert", "../shaders/particles.frag");
     particlesShader();
 
@@ -38,7 +70,23 @@ int main() {
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glBindVertexArray (vao);
+        glDrawArrays (GL_POINTS, 0, n_particles);
+
+        if (glfwGetKey (window, GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose (window, 1);
+        }
+
+        // insert location binding code here
+        glBindAttribLocation (particlesShader, 0, "position");
+        glBindAttribLocation (particlesShader, 1, "velocity");
+
+        //glLinkProgram (particlesShader);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
--- cpp/SPH_cpp/shaders/particles.frag
+++ cpp/SPH_cpp/shaders/particles.frag
@@ -1,7 +1,8 @@
 #version 330 core
 
+
 out vec4 color;
 
 void main() {
-    color = vec4(1.0, 1.0, 1.0, 1.0);
+    color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
 }
