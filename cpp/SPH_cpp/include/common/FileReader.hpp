5c890b1bf6094cd054b43dab50afd2f0fa68b743
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-02-02 13:08:06 +0100

    Add FileReader class

---
+++ cpp/SPH_cpp/include/common/FileReader.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+/// @brief //todo add brief description to FileReader
+/// @author benjamin
+class FileReader {
+public:
+    static const std::string ReadFromFile(std::string fileName);
+};
---
+++ cpp/SPH_cpp/src/common/FileReader.cpp
@@ -0,0 +1,22 @@
+#include "common/FileReader.hpp"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+const std::string FileReader::ReadFromFile(std::string fileName) {
+    std::ifstream ifs(fileName.c_str());
+    std::string out;
+
+    if (ifs.is_open()) {
+        std::string s;
+        while (std::getline(ifs, s)) {
+            out += s + "\n";
+        }
+    } else {
+        std::cerr << "Could not open file" << std::endl;
+    }
+
+    ifs.close();
+    return out;
+}
